ec9062eee1674b5973c26b7f440266a42d470a4c
dfish19 <dfish19@yahoo.com>
2022-08-04 12:28:36 -0500

    change again

--- MethodExercise2/Program.cs
+++ MethodExercise2/Program.cs
@@ -4,20 +4,27 @@ namespace MethodsExercise
 {
 
     class Program
-{
-    public static int Sum(int num1, int num2)
     {
-        var answer = num1 + num2;
-        return answer;
-        var amountOfCookies = Sum(3, 5);
         
+
+             static int Sum(int num1, int num2)
+            {
+                var answer = num1 + num2;
+                return answer;
+                
+
+            }
+             static int Multiply(int num1, int num2)
+            {
+                var answer = num1 * num2;
+                return answer;
+
+            static void Main(string[] args)
+            {
+                var amountOfCookies = Sum(3, 5);
+
+
+            }
+        }
     }
-    public static int Multiply(int num1, int num2)
-    {
-        var answer = num1 * num2;
-        return answer;
-        var amountOfCookies = Multiply(3, 5);
-        
-    }
-}
 }
