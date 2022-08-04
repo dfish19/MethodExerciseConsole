e9eb62148cbd5862d1fe220e7a2b02f7af5263c9
dfish19 <dfish19@yahoo.com>
2022-08-04 00:37:07 -0500

    new location

---
+++ MethodExercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //-----------------Exercise 1------------------------------
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName} What car do you drive?");
+            var car = Console.ReadLine();
+
+            Console.WriteLine($"{car} are nice cars. What football team do you like?");
+            var team = Console.ReadLine();
+
+            Console.WriteLine($"I think {team} will win the Super Bowl this year!");
+        }
+    }
+}
