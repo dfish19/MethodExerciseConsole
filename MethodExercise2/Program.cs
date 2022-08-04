893ad95d1eb81cd01a36e023b18b24122b85bccd
dfish19 <dfish19@yahoo.com>
2022-08-04 10:26:27 -0500

    initial commit

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods Exercise
+{
+
+    class Program
+{
+    public static int Sum(int num1, int num2)
+    {
+        var answer = num1 + num2;
+        return answer;
+        var amountOfCookies = Sum(3, 5);
+        
+    }
+    public static int Multiply(int num1, int num2)
+    {
+        var answer = num1 * num2;
+        return answer;
+        var amountOfCookies = Multiply(3, 5);
+        
+    }
+}
+}
